9112df275b42e680a45add648b7f76076d4939ea
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2023-02-16 16:30:11 +0200

    add test code from discussion after lecture

---
+++ src/test/A.java
@@ -0,0 +1,13 @@
+package test;
+
+public class A {
+    /*private*/ int f/* = 3*/;
+
+    public int getF() {
+        return f;
+    }
+
+    public void setF(int f) {
+        this.f = f;
+    }
+}
