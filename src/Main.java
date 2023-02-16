81947f9b29bd4c7b76acd0edace8d3864a291877
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2023-02-16 16:10:38 +0200

    add EncapsulationInheritancePolymorphism

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(args[1]);
         System.out.println("Hello world!");
 //        System.out.println(1);
     }
---
+++ src/encapsulationInheritancePolymorphism/Student.java
@@ -0,0 +1,10 @@
+package encapsulationInheritancePolymorphism;
+
+public class Student {
+    private String name;
+    private String surname;
+
+    public String getName() {
+        return name;
+    }
+}
