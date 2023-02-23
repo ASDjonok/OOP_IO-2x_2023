53a9d204386b4aa77aaacf6778d370db8d46a5c4
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2023-02-23 14:30:35 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(args[1]);
+        System.out.println(args[2]);
         System.out.println("Hello world!");
 //        System.out.println(1);
     }
