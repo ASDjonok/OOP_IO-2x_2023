bb2c8bd1a022fc69b59e3fe71fa4b418a63dc0fc
Maxim Papko <125305022+idkWhatUserNameToUse@users.noreply.github.com>
2023-03-23 20:30:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Laba1
+{
+    public static void Main(string[] args)
+    {
+        var a = 1; 
+        var b = 0;
+        var n = 2;
+        var m = 2;
+        var c = 4;
+    }
+}
