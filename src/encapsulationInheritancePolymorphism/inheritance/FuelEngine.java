cf3d055f64caa896b21354b5195755c6ca232984
Rhinemann <72938564+Rhinemann@users.noreply.github.com>
2023-03-23 08:57:30 +0200

    Delete src directory

--- src/encapsulationInheritancePolymorphism/inheritance/FuelEngine.java
+++
@@ -1,5 +0,0 @@
-package encapsulationInheritancePolymorphism.inheritance;
-
-public class FuelEngine extends Engine {
-    private String fuelType;
-}
--- src/encapsulationInheritancePolymorphism/inheritance/Main.java
+++
@@ -1,8 +0,0 @@
-package encapsulationInheritancePolymorphism.inheritance;
-
-public class Main {
-    public static void main(String[] args) {
-        ElectricEngine electricEngine = new ElectricEngine(); // створення нового об'єкту (екземпляру) класу ElectricEngine
-        FuelEngine fuelEngine = new FuelEngine();
-    }
-}
