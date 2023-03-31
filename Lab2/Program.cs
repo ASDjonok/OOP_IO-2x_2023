8b517d136f9c36dd3facfa432cc74ed91d3618dd
idkWhatUserNameToUse <papko.maxim@lll.kpi.ua>
2023-03-31 21:10:47 +0300

    lab2 with error

---
+++ Lab2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Laba2
+{
+    
+
+    public static void Main(string[] args)
+    {
+        const int a = 2;
+        int[][] b = new[]
+        {
+            new int[] { 1, 2, 8 },
+            new int[] { 3, 4, 5 },
+            new int[] { 6, 7, 9 }
+        };
+        calculation(b, a);
+    }
+
+    private static int[][] calculation(int[][] b, int a)
+    {
+        int[][]c = new int[b.Length][b[0].Length];
+        for (int i = 0; i < b.Length; i++)
+        {
+            c[i] = new int[b[i].Length];
+            for (int j = 0; j < b[i].Length; j++)
+            {
+                c[i][j] = b[i][j] * a;
+            }
+        }
+        return c;
+    }
+}
